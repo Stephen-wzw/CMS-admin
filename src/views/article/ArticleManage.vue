b9a0ca6f85a9928a97923d7f7fcbdb17ef8ef9ec
Stephen-wzw <wzw15292257101@163.com>
2021-05-08 12:08:59 +0800

    搜索部分

--- src/views/article/ArticleManage.vue
+++ src/views/article/ArticleManage.vue
@@ -1,15 +1,24 @@
 <template>
-  <div class="article-manage">
-    文章管理
+  <div class="article-manage wrap">
+    <bread-crumb>文章管理</bread-crumb>
+    <div class="body">
+      <filter-form></filter-form>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
+import BreadCrumb from "components/common/BreadCrumb";
+import FilterForm from "./childComps/FilterForm"
 
-}
+export default {
+  components: {
+    BreadCrumb,
+    FilterForm,
+  },
+};
 </script>
 
-<style>
+<style scoped>
 
 </style>
