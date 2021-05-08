068e79a0257489ab315555b010aa2859293f6877
Stephen-wzw <wzw15292257101@163.com>
2021-05-08 12:06:54 +0800

    文章组件

---
+++ src/views/article/ArticleEdit.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="article-manage">
+    <bread-crumb>文章管理</bread-crumb>
+    <div class="ss"></div>
+  </div>
+</template>
+
+<script>
+import BreadCrumb from "components/common/BreadCrumb";
+
+export default {
+  components: {
+    BreadCrumb,
+  },
+};
+</script>
+
+<style></style>
