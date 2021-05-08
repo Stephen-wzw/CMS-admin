54e455fd161456b944b109c1c913e44fdae06751
Stephen-wzw <wzw15292257101@163.com>
2021-05-08 12:06:02 +0800

    公共组件

---
+++ src/components/common/BreadCrumb.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="bread-crumb">
+    <slot>面板屑</slot>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.bread-crumb {
+  background-color: #fff;
+
+  font-size: 18px;
+  font-weight: 300;
+  color: #333;
+
+  height: 55px;
+  line-height: 55px;
+  padding: 0 25px;
+
+  user-select: none;
+}
+</style>
