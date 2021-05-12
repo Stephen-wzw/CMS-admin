35141adfd934451e3c9922647ec56542afae5edc
Stephen-wzw <wzw15292257101@163.com>
2021-05-12 19:37:59 +0800

    引入标签

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -24,6 +24,7 @@ import {
   Table,
   TableColumn,
   Avatar,
+  Tag,
 } from "element-ui";
 
 const element = {
@@ -49,6 +50,8 @@ const element = {
     Vue.use(DatePicker);
 
     Vue.use(Backtop);
+
+    Vue.use(Tag)
 
     Vue.use(Form);
     Vue.use(FormItem);
