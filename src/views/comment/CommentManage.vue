a51f7f0aef454134c0d9dfdcf3932ba27ad0e140
Stephen-wzw <wzw15292257101@163.com>
2021-05-13 09:15:37 +0800

    评论管理

--- src/views/comment/CommentManage.vue
+++ src/views/comment/CommentManage.vue
@@ -1,15 +1,23 @@
 <template>
   <div class="comment-manage">
     <bread-crumb>评论管理</bread-crumb>
+    <div class="body">
+      <comment-filter></comment-filter>
+      <comment-table></comment-table>
+    </div>
   </div>
 </template>
 
 <script>
 import BreadCrumb from "components/common/BreadCrumb";
+import CommentFilter from "./childComps/CommentFilter";
+import CommentTable from "./childComps/CommentTable";
 
 export default {
   components: {
     BreadCrumb,
+    CommentFilter,
+    CommentTable,
   },
 };
 </script>
