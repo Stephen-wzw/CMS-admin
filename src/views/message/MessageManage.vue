0607f45d7f46c3af0584f14f91d52fdb6d252fe5
Stephen-wzw <wzw15292257101@163.com>
2021-05-13 09:15:54 +0800

    留言管理

--- src/views/message/MessageManage.vue
+++ src/views/message/MessageManage.vue
@@ -1,15 +1,23 @@
 <template>
   <div class="message-manage">
     <bread-crumb>留言管理</bread-crumb>
+    <div class="body">
+      <message-filter></message-filter>
+      <message-table></message-table>
+    </div>
   </div>
 </template>
 
 <script>
 import BreadCrumb from "components/common/BreadCrumb";
+import MessageFilter from "./childComps/MessageFilter";
+import MessageTable from "./childComps/MessageTable";
 
 export default {
   components: {
     BreadCrumb,
+    MessageFilter,
+    MessageTable,
   },
 };
 </script>
