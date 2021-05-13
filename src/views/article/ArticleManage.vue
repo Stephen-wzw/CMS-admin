da8f63e44e50babfb47a4908236f35bc9346e8c2
Stephen-wzw <wzw15292257101@163.com>
2021-05-13 09:15:03 +0800

    解决表格无法渲染数据的问题

--- src/views/article/ArticleManage.vue
+++ src/views/article/ArticleManage.vue
@@ -2,7 +2,7 @@
   <div class="article-manage wrap">
     <bread-crumb>文章管理</bread-crumb>
     <div class="body">
-      <filter-form></filter-form>
+      <article-filter></article-filter>
       <article-table></article-table>
     </div>
   </div>
@@ -10,13 +10,13 @@
 
 <script>
 import BreadCrumb from "components/common/BreadCrumb";
-import FilterForm from "./childComps/FilterForm";
+import ArticleFilter from "./childComps/ArticleFilter";
 import ArticleTable from "./childComps/ArticleTable";
 
 export default {
   components: {
     BreadCrumb,
-    FilterForm,
+    ArticleFilter,
     ArticleTable,
   },
 };
