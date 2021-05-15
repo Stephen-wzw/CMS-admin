919acb5e18449268ac4b8a15546cb0472efa6832
Stephen-wzw <wzw15292257101@163.com>
2021-05-15 17:43:32 +0800

    引入全局事件总线

--- src/main.js
+++ src/main.js
@@ -9,6 +9,9 @@ import element from "./plugins/element";
 import "element-ui/lib/theme-chalk/index.css";
 Vue.use(element);
 
+// 引入事件总线
+Vue.prototype.$EventBus = new Vue()
+
 Vue.config.productionTip = false;
 
 new Vue({
